fbc3f549d3c753fceb05f0511aaec9284d8ae446
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-09-06 17:47:15 +0200

    the old version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [show, setShow] = useState(true);
+  const initialW = window.innerWidth;
+  const initialH = window.innerHeight;
+  const [windowSize, setWindowSize] = useState({
+    windowW: initialW,
+    windowH: initialH,
+  });
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize({
+        windowH: window.innerHeight,
+        windowW: window.innerWidth,
+      });
+    }
+    window.addEventListener("resize", handleWindowResize);
+    //handleWindowResize();
+
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, [windowSize]);
+
+  const display = (
+    <div className="App">
+      <h1>Window resize </h1>
+      <br />
+      <h2>The window width is : {windowSize.windowW}px</h2>
+      <h2>The window height is : {windowSize.windowH}px</h2>
+    </div>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setShow(show => !show)}>
+        Hide/Show Window Size
+      </button>
+      {show ? display : null}
     </div>
   );
 }
