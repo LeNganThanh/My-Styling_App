c156a7b0f1260b7d8be24d900dc3fb575f180db8
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-09-06 17:50:16 +0200

    last styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import "./App.css";
+//import "./App.css";
 import React, { useState, useEffect } from "react";
+//get css from module
+import StyleCSS from "./modules/Style.module.css";
 
 function App() {
   const [show, setShow] = useState(true);
@@ -24,22 +26,24 @@ function App() {
   }, [windowSize]);
 
   const display = (
-    <div className="App">
-      <h1>Window resize </h1>
-      <br />
-      <h2>The window width is : {windowSize.windowW}px</h2>
-      <h2>The window height is : {windowSize.windowH}px</h2>
+    <div className={StyleCSS.container}>
+      <h1 className={StyleCSS.h1style}>Window resize </h1>
+      <h2 className={StyleCSS.h2styleP1}>
+        The window width is : {windowSize.windowW}px
+      </h2>
+      <h2 className={StyleCSS.h2styleP2}>
+        The window height is : {windowSize.windowH}px
+      </h2>
     </div>
   );
 
   return (
-    <div className="App">
-      <button onClick={() => setShow(show => !show)}>
+    <div className={StyleCSS.container}>
+      <button className={StyleCSS.btn} onClick={() => setShow(show => !show)}>
         Hide/Show Window Size
       </button>
       {show ? display : null}
     </div>
   );
 }
-
 export default App;
