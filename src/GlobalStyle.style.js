0908e33d6aab23d73a0c139ab641bf5f6d20e638
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-09-07 09:46:26 +0200

    adding height to image

--- src/GlobalStyle.style.js
+++ src/GlobalStyle.style.js
@@ -4,6 +4,7 @@ body{
     background: url(./OWZ.jpeg) no-repeat;
     background-size: cover;
     object-fit: cover;
+    height: 100vh;
     background-position: fixed;
     margin: 0;
     padding: 0px;
